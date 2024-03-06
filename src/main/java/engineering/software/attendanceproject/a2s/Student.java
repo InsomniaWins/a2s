5278a28f9026d17e5b5f51658e488e18c499dc2e
InsomniaWins <57158000+InsomniaWins@users.noreply.github.com>
2024-03-06 17:44:16 -0600

    Added StudentList and Student classes

---
+++ src/main/java/engineering/software/attendanceproject/a2s/Student.java
@@ -0,0 +1,15 @@
+package engineering.software.attendanceproject.a2s;
+
+public class Student {
+
+    private String firstName;
+    private char middleInitial;
+    private String lastName;
+
+    public Student(String firstName, char middleInitial, String lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+    }
+
+}
---
+++ src/main/java/engineering/software/attendanceproject/a2s/StudentList.java
@@ -0,0 +1,33 @@
+package engineering.software.attendanceproject.a2s;
+
+
+import java.util.ArrayList;
+
+
+/*
+container class of students for attendance
+ */
+
+public class StudentList {
+
+    private ArrayList<Student> students;
+
+
+    // returns true if student was added to list
+    // returns false if student was already in list
+    public boolean addStudent(Student student) {
+
+        if (students.contains(student)) {
+            return false;
+        }
+
+        students.add(student);
+        return true;
+    }
+
+    public ArrayList<Student> getStudents()
+    {
+        return students;
+    }
+
+}
